eb3555f5ff8c68149a47db5c6e76734fd11b99d6
aneeshansaricloud1 <aneeshansaricloud1@gmail.com>
2022-01-31 07:43:23 +0000

    added

--- single-module/src/test/java/com/example/TestGreeter.java
+++ single-module/src/test/java/com/example/TestGreeter.java
@@ -30,4 +30,12 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+    @Test
+  public void greetShouldIncludeGreetingMsg() {
+    String someone = "World";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
+
+
 }
